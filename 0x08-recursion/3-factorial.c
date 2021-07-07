925fbad7b839efe29beea801a83ec094398de2f4
thenedum <okpalachifrancis@gmail.com>
2021-07-07 12:01:27 -0700

    factorial correction

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -18,24 +18,4 @@ int factorial(int n)
 	fac = factorial(n - 1);
 
 	return (fac * n);
-}#include "holberton.h"
-
-/**
- * factorial - compute the factorial of a given number.
- * @n: the number
- * Return: factorial number
- */
-int factorial(int n)
-{
-	int fac = 1;
-
-	if (n < 0)
-		return (-1);
-
-	else if (!n)
-		return (1);
-
-	fac = factorial(n - 1);
-
-	return (fac * n);
 }
